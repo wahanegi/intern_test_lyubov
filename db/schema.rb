aef829a9ffb874e98d6b684c54acd04d02ebbeec
LyubovP <lyubovpidoshva@mail.com>
2023-01-17 06:11:33 +0200

    add navbar and configure devise controller

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_16_145042) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "nikname"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
