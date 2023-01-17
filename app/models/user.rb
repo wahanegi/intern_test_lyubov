4e28638b4eda98f0280763f918099effe6ab8850
LyubovP <lyubovpidoshva@mail.com>
2023-01-17 12:18:48 +0200

    add validation user - email, nikname, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nikname, presence: true, uniqueness: true, length: { minimum: 3 }, format: { with: /\A[A-Za-z][A-Za-z0-9_-]{3,19}\z/ , message: " Please use only characters -_ "}
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, uniqueness: true }
+  validates :password, format: { with: /\A\S{8,128}\z/, allow_nil: true }
+
 end
